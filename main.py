bb6f65da3509940c182a34dc14ad044c1c0bc47a
Frederik Feldmeier <74424903+elfeldos@users.noreply.github.com>
2023-09-30 19:32:12 +0200

    Update main.py
    added basic pygame screen

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
-import pygame
 # import pygame 
+import pygame, sys
+
+# initialize pygame
+pygame.init()
+# create a screen (width, height) - in a tuple
+screen = pygame.display.set_mode((400, 500))
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            # .quit is opposite of .init & sys.exit to make sure everything will be closed
+            pygame.quit()
+            sys.exit()
+    screen.fill((175, 215, 70))
+    # draw all our elements
+    pygame.display.update()
+    # limiting the while loop because of different speed of computers / 60 = framerate
+    clock.tick(60)
