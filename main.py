5d24535018d5f3e01e80f57b0ec639c3c4146972
Frederik Feldmeier <74424903+elfeldos@users.noreply.github.com>
2023-09-30 21:43:34 +0200

    Update main.py
    creating snake and fruit

--- main.py
+++ main.py
@@ -1,11 +1,43 @@
 # import pygame 
-import pygame, sys
+import pygame, sys, random
+from pygame.math import Vector2
+
+class SNAKE:
+    def __init__(self):
+        self.body = [Vector2(5, 10), Vector2(6, 10), Vector2(7, 10)]
+
+    def draw_snake(self):
+        for block in self.body:
+            x_pos = int(block.x * cell_size)
+            y_pos = int(block.y * cell_size)
+            # create a rectangle
+            block_rect = pygame.Rect(x_pos, y_pos, cell_size, cell_size)
+            # draw rectangle
+            pygame.draw.rect(screen, pygame.Color('Magenta'), block_rect)
+
+class FRUIT:
+    def __init__(self):
+        # crate an x and y position
+        self.x = random.randint(0, cell_number - 1)
+        self.y = random.randint(0, cell_number - 1)
+        self.pos = Vector2(self.x, self.y)
+    
+    def draw_fruit(self):
+        # create a rectangle
+        fruit_rect = pygame.Rect(int(self.x * cell_size), int(self.y * cell_size), cell_size, cell_size)
+        # draw the rectangle
+        pygame.draw.rect(screen, (126, 166, 114), fruit_rect)
 
 # initialize pygame
 pygame.init()
+cell_size = 40
+cell_number = 20
 # create a screen (width, height) - in a tuple
-screen = pygame.display.set_mode((400, 500))
+screen = pygame.display.set_mode((cell_size * cell_number, cell_size * cell_number))
 clock = pygame.time.Clock()
+
+fruit = FRUIT()
+snake = SNAKE()
 
 while True:
     for event in pygame.event.get():
@@ -14,6 +46,10 @@ while True:
             pygame.quit()
             sys.exit()
     screen.fill((175, 215, 70))
+    # draw fruit
+    fruit.draw_fruit()
+    # draw snake
+    snake.draw_snake()
     # draw all our elements
     pygame.display.update()
     # limiting the while loop because of different speed of computers / 60 = framerate
